1654a2a6cbada25f13d9a31182db20518e628323
Josh Lear <joshrlear@gmail.com>
2020-08-05 16:51:17 -0700

    changed h1

--- yim-verify-form-azure/src/App.js
+++ yim-verify-form-azure/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Hope this works...</h1>
+        <h1>Does this work?</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
