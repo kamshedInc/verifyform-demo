92d209ccd8c1a2f1852a6c78cde43e7935845d1a
Josh Lear <joshrlear@gmail.com>
2020-08-13 12:22:48 -0700

    update config file

---
+++ yim-verify-form-azure/src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "dLVerifyRequest": process.env.REACT_APP_DL_VERIFY_SUBMISSION,
+    "requestSmsCode": process.env.REACT_APP_GET_SMS_CODE,
+    "verifySmsCode": process.env.REACT_APP_VERIFY_SMS_CODE
+}
