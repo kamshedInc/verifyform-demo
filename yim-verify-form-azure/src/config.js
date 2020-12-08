897c4c0f635d4d1857bcd8e9e7ffa3313a10ccce
Josh Lear <joshrlear@gmail.com>
2020-12-08 10:39:45 -0800

    remove unneed files

--- yim-verify-form-azure/src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "dLVerifyRequest": process.env.REACT_APP_DL_VERIFY_SUBMISSION,
-    "requestSmsCode": process.env.REACT_APP_GET_SMS_CODE,
-    "verifySmsCode": process.env.REACT_APP_VERIFY_SMS_CODE
-}
