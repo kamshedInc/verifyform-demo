102570d591f133bcaf697691954033db0dc81d24
joshrlear <joshrlear@gmail.com>
2020-12-20 14:32:43 -0800

    cleaned modules and removed rear cam capability

--- src/helperFunctions/captureVideo.js
+++ src/helperFunctions/captureVideo.js
@@ -1,11 +1,7 @@
-export default function captureVideo(video,res,step) {
-    const rear = step === 2 
-        ? true
-        : false
-
+export default function captureVideo(video,res) {
     navigator.mediaDevices.getUserMedia({ 
         audio: false, 
-        video: { facingMode: (rear? "environment" : "user") }
+        video: true
     })
     .then(stream => {
         video.srcObject = stream
